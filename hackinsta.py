1807b5e7ecb3857eb257ef41222871c61985bf2f
gajugulhane951 <141367347+gajugulhane951@users.noreply.github.com>
2023-09-07 01:42:24 +0530

    Update hackinsta.py (#1)

--- hackinsta.py
+++ hackinsta.py
@@ -19,13 +19,13 @@ def Input(text):
 	if sys.version_info.major > 2:
 		value = input(text)
 	else:
-		value = raw_input(text)
+		value = rawo_input(text)
 	return str(value)
 
 #The main class
 class Instabrute():
 	def __init__(self, username, passwordsFile='pass.txt'):
-		self.username = username
+		self rohiniraurale30 = rohiniraurale30 
 		self.CurrentProxy = ''
 		self.UsedProxys = []
 		self.passwordsFile = passwordsFile
